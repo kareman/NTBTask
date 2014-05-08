1b54165e68513edb518518379930df3de2120acd
Kåre Morstøl <karemorstol@iMacen.local>
2014-05-08 20:36:34 +0200

    Added some much-needed documentation.

--- NTBTask.h
+++ NTBTask.h
@@ -1,6 +1,5 @@
 //
 //  NTBTask.h
-//  Copy files
 //
 //  Created by Kåre Morstøl on 30/03/14.
 //  Copyright (c) 2014 NotTooBad Software. All rights reserved.
@@ -10,22 +9,71 @@
 
 #import <Foundation/Foundation.h>
 
+/**
+ *  A class for running another program asynchronously in a separate process, sending input and receiving output from it.
+ */
 @interface NTBTask : NSObject {
 
   @private
 	NSTask *_task;
 }
 
+/**
+ *  Text encoding for the task's input and output. The default is NSUTF8StringEncoding.
+ */
 @property NSStringEncoding encoding;
 
+/**
+ *  Invoked when more output is ready. Can happen many times while the task is running.
+ */
 @property (nonatomic, strong) void (^outputHandler)(NSString *);
+
+/**
+ *  Invoked when more error output is ready. Can happen many times while the task is running.
+ */
 @property (nonatomic, strong) void (^errorHandler)(NSString *);
+
+/**
+ *  Invoked when the task is completed.
+ *
+ *  This block is not guaranteed to be fully executed prior to waitUntilExit returning.
+ */
 @property (nonatomic, strong) void (^completionHandler)(NTBTask *);
 
+/**
+ *  Initialises a new task.
+ *
+ *  @param launchPath  The full path to the executable.
+ */
 - (instancetype)initWithLaunchPath:(NSString *)launchPath;
+
+/**
+ *  Launches the task in its own process, and returns before the task is finished.
+ *  @throws  NSInvalidArgumentException if the launch path has not been set or is invalid or if it fails to create a process.
+ */
 - (void)launch;
+
+/**
+ *  Writes text to the standard input of the task. Works both before and after it is launched.
+ *  @warning When used together with "launch" the last write must use "writeAndCloseInput:" instead, otherwise the task will never end.
+ *
+ *  @param input  The text to send to the task.
+ */
 - (void)write:(NSString *)input;
+
+/**
+ *  Writes text to the standard input of the task, and then closes standard input. Works both before and after the task is launched.
+ *
+ *  @param input  The text to send to the task.
+ */
 - (void)writeAndCloseInput:(NSString *)input;
+
+/**
+ *  Launches the task, waits until it's finished, and returns with the output.
+ *  @warning  Any existing output handler will be replaced.
+ *
+ *  @return  The standard output from the task. Also includes error output if no errorHandler is defined.
+ */
 - (NSString *)waitForOutputString;
 
 @end
